a46473e21db542ee9dddf64231b0e5708669693b
SHAMIL MUHYIN K <shamilmuhyink@gmail.com>
2024-10-01 23:27:12 +0530

    Defined entity and repositories to add user

--- FinanceTransactionRecord/src/main/java/com/fincords/model/AppUser.java
+++ FinanceTransactionRecord/src/main/java/com/fincords/model/AppUser.java
@@ -1,7 +1,29 @@
 package com.fincords.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity(name="users")
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="users")
 public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", initialValue = 000001, allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Long mobileNo;
+
+    @Column(nullable = false)
+    private String password;
 }
---
+++ FinanceTransactionRecord/src/main/java/com/fincords/repository/AppUserRepository.java
@@ -0,0 +1,9 @@
+package com.fincords.repository;
+
+import com.fincords.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+}
