f42104da05c41c02616f7c51c81252ed8eae7999
Shamil Muhyin K <shamil.muhyin@ACTCORP.LOCAL>
2024-10-01 12:53:31 +0530

    Initializing springboot project

---
+++ FinanceTransactionRecord/src/main/java/com/fincords/FinanceTransactionRecordApplication.java
@@ -0,0 +1,13 @@
+package com.fincords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTransactionRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTransactionRecordApplication.class, args);
+	}
+
+}
---
+++ FinanceTransactionRecord/src/test/java/com/fincords/FinanceTransactionRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTransactionRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
